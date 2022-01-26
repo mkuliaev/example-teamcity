2ebe51a374fd4e4f58054c39984cd1357ae18b3f
Pitonixx <pitonix@ya.ru>
2022-01-26 17:41:59 +0300

    add hunter

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -7,4 +7,7 @@ public class Welcomer{
 	public String sayFarewell() {
 		return "Farewell, good hunter. May you find your worth in waking world.";
 	}
+	public String saySomePhrase() {
+		return "Every hunter want to know, where sit the pheasant"
+	}
 }
