9b365cba152917220548a0b4ad16bbfdbda51ec6
gasan <abduragimov.ga@gmail.com>
2022-03-21 17:44:48 +0500

    change status message

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -8,6 +8,6 @@ public class Welcomer{
 		return "Farewell, good hunter. May you find your worth in waking world.";
 	}
 	public String sayStatus() {
-		return "Good Status";
+		return "Good status";
 	}
 }
