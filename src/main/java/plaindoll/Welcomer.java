788e4f81142e1b21710b71b814916ac651c763e0
iv-art074 <87374285+iv-art074@users.noreply.github.com>
2022-03-26 15:20:44 +1000

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -2,7 +2,7 @@ package plaindoll;
 
 public class Welcomer{
 	public String sayWelcome() {
-		return "Welcome home, good hunter. What is it your desire?";
+		return "Welcome home, good Great_hunter. What is it your desire?";
 	}
 	public String sayFarewell() {
 		return "Farewell, good Great_hunter. May you find your worth in waking world.";
