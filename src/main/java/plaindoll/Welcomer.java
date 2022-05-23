6fd42669404288d22055418a628613493594389c
pavlovob <pavlovob@gmail.com>
2022-05-23 12:36:19 +0300

    Added sayCustom method class realization

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -7,4 +7,7 @@ public class Welcomer{
 	public String sayFarewell() {
 		return "Farewell, good hunter. May you find your worth in waking world.";
 	}
+	public String sayCustom() {
+		return "Cutom method class realization for a good hunter. What else do you need, Netology?";
+	}	
 }
