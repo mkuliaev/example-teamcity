be00356fba738c9fe0af8e4c3b1c5358e9ff225a
kokobops <91221547+kokobops@users.noreply.github.com>
2022-06-11 16:19:14 +0200

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -7,7 +7,4 @@ public class Welcomer{
 	public String sayFarewell() {
 		return "Farewell, good hunter. May you find your worth in waking world.";
 	}
-	public String sayHunter() {
-		return "The poor hunter tired of doing homework";
 	}
-}
