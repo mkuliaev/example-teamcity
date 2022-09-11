138aff0cb615d88a331ec178df96383bbd1ff1eb
Igor S <906a@mail.ru>
2022-09-11 19:34:57 +0000

    Add sayHunter

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+	public String sayHunter(){
+		return "...hunter Sebastian who was sleeping on the attic...";
+	}
 }
