9cad13f549e4a29f526af0dc18889679eb929da7
Timur Ahmetzyanov <dijer@mail.ru>
2022-11-27 23:28:25 +0300

    fix2

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -32,6 +32,6 @@ public class WelcomerTest {
 	}
 	@Test
 	public void welcomerSaysWithHunter() {
-		assertThat(welcomer.saySome(), containsString("hunter"));
+		assertThat(welcomer.sayHunter(), containsString("hunter"));
 	}
 }
