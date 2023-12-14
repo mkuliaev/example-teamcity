e8aed0098377a9a4864465ddf482983f59ebe0fc
Andrey Drutskoy <108830555+Muroway@users.noreply.github.com>
2023-12-14 22:17:08 +0300

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+	public String sayHunterST(){
+		return "You are now leaving Fear & Loathing, hunter. Pop 0";
+	}
 }
