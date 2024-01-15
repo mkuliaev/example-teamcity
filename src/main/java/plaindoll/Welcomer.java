d65d0d2d8062eacef8d3a5c005772f62e874e89c
Kul-RB <53901269+Kul-RB@users.noreply.github.com>
2024-01-15 15:25:54 +0300

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,7 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
-	public String sayHanter(){
+	public String sayHunter(){
 		return "I know you are a good hunter";
 	}
 }
